edc9344b6722fab888af5ac6a969c0e52e39d014
lqkxs3608 <67536525+lqkxs3608@users.noreply.github.com>
2020-10-02 12:33:35 +0800

    Update 52pojie_sign.py

--- 52pojie_sign.py
+++ 52pojie_sign.py
@@ -4,25 +4,22 @@ from pyquery import PyQuery as pq
 import os
 
 
-def main():
-    url = 'https://www.52pojie.cn/home.php?mod=task&do=draw&id=2'
-    url1 = 'https://www.52pojie.cn/home.php?mod=task&do=apply&id=2'
-    headers = {'Cookie': cookie,
-               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4265.0 Safari/537.36 Edg/87.0.644.4'}
-    requests.get(url1, headers=headers)
-    req = requests.get(url, headers=headers).text
+cookie = os.environ["cookie"]
+sckey = os.environ["SCKEY"]
+url = 'https://www.52pojie.cn/home.php?mod=task&do=draw&id=2'
+url1 = 'https://www.52pojie.cn/home.php?mod=task&do=apply&id=2'
+headers = {'Cookie': cookie,
+           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4265.0 Safari/537.36 Edg/87.0.644.4'}
+requests.get(url1, headers=headers)
+req = requests.get(url, headers=headers).text
     
-    doc = pq(req)
-    msg = doc('.vwmy a').text() + '\t' + doc('#messagetext p').text()
-    print(msg)
-    if not cookie:
-        print('cookie为空')
-    if sckey:
-        send_url = f'https://sc.ftqq.com/{sckey}.send?text={msg}'
-        requests.get(send_url)
+doc = pq(req)
+msg = doc('.vwmy a').text() + '\t' + doc('#messagetext p').text()
+print(msg)
+if not cookie:
+    print('cookie为空')
+if sckey:
+    send_url = f'https://sc.ftqq.com/{sckey}.send?text={msg}'
+    requests.get(send_url)
 
 
-if __name__ == '__main__':
-    cookie = os.environ["cookie"]
-    sckey = os.environ["SCKEY"]
-    main()
