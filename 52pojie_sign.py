46ed0509077dcde4eeeb513cd8834f8b1bfa6976
lqkxs3608 <67536525+lqkxs3608@users.noreply.github.com>
2020-10-02 15:46:39 +0800

    Update 52pojie_sign.py

--- 52pojie_sign.py
+++ 52pojie_sign.py
@@ -7,10 +7,10 @@ cookie = input("cookie")
 sckey = input("sckey")
 url = 'https://www.52pojie.cn/home.php?mod=task&do=draw&id=2'
 url1 = 'https://www.52pojie.cn/home.php?mod=task&do=apply&id=2'
-headers = {'Cookie': cookie,
+headers = {
            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4265.0 Safari/537.36 Edg/87.0.644.4'}
 requests.get(url1, headers=headers)
-req = requests.get(url, headers=headers).text
+req = requests.get(url, headers=headers,cookie=cookie).text
     
 doc = pq(req)
 msg = doc('.vwmy a').text() + '\t' + doc('#messagetext p').text()
